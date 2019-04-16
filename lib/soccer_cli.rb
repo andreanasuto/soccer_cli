4c6d37785822bcd732b57ff77681e7c42a03da5d
andreanasuto <andrea.nasuto@gmail.com>
2019-04-16 13:41:46 -0400

    built a fake cli ui to test functionalities

--- lib/soccer_cli.rb
+++ lib/soccer_cli.rb
@@ -1,2 +1,3 @@
 require_relative "./soccer_cli/version"
 require_relative "./soccer_cli/cli"
+require_relative "./soccer_cli/competition"
--- lib/soccer_cli/cli.rb
+++ lib/soccer_cli/cli.rb
@@ -1,5 +1,16 @@
 class CommandLineInterface
   def call
-    puts "Choose your team"
+    puts "Choose your competition"
+    list_competitions
+    list_standings
+  end
+
+  def list_competitions
+    puts "1. Liga / 2.Premier League..."
+  end
+
+  def list_standings
+    puts "Here's current Premier League standings"
+    puts "1.City 2.Liverpool.."
   end
 end
