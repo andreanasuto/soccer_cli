dec629e5ebdd7a4501214fadddc9697b98079013
andreanasuto <andrea.nasuto@gmail.com>
2019-04-16 21:16:12 -0400

    self.scraper_team_profile method written

--- lib/soccer_cli/scraper.rb
+++ lib/soccer_cli/scraper.rb
@@ -3,17 +3,30 @@ require 'nokogiri'
 require 'open-uri'
 
 class Scraper
-  def self.scraper_competition(url)
+  def self.scraper_team_table(url)
     output = []
-    doc = Nokogiri::HTML(open("https://www.transfermarkt.us/serie-a/startseite/wettbewerb/IT1"))
+    doc = Nokogiri::HTML(open(url))
     table = doc.css('.responsive table').last
 
     table.search('tr').each do |tr|
       profile = {
-        :name => tr.search('a').text,
+        :name => tr.css('td.no-border-links.hauptlink a').text
+        :points => tr.css('td.zentriert')[7].text
+        :url => tr.css('td.no-border-links.hauptlink a').attr('href')
       }
       output << profile
-    end
+  end
 
-    output
+  def self.scraper_team_profile(url)
+    doc = Nokogiri::HTML(open(url))
+    first = doc.css('.dataBottom > .dataDaten').first
+    second = @doc.css('.dataBottom > .dataDaten').last
+
+    profile_2 = {
+      :average_age => first.search('p')[1].css('span.dataValue').text.strip
+      :stadium => second.search('p')[1].css('span.dataValue a').text.strip
+      :mkt_value => doc.css('.dataMarktwert a').children[1].text
+    }
+  end
+
 end
