8b0a6d36340c7923223fc12be048d8d5c1b2e0a3
andreanasuto <andrea.nasuto@gmail.com>
2019-04-16 23:27:47 -0400

    select_team change

--- lib/soccer_cli/cli.rb
+++ lib/soccer_cli/cli.rb
@@ -2,6 +2,8 @@ require_relative './soccer_cli/team'
 require_relative './soccer_cli/scraper'
 
 class CommandLineInterface
+  url = "https://www.transfermarkt.us/serie-a/tabelle/wettbewerb/IT1/saison_id/2018"
+
   def run
     puts "Welcome to Serie A CLI"
     make_teams
@@ -9,7 +11,7 @@ class CommandLineInterface
     select_team
   end
 
-  def make_teams
+  def make_teams(url)
     hash_profile = Scraper.scraper_table(url)
     Team.new(hash_profile)
     i = 0
@@ -31,8 +33,11 @@ class CommandLineInterface
     input = ""
     puts "Select team by typing its table ranking"
     input = gets.strips
-    team_selected = Team.find_by_ranking(input)
-
+    team = Team.find_by_ranking(input)
+    puts "Here some details:"
+    puts "#{team.name} is currently value $#{team.value}mn"
+    puts "#{team.name} players average age is #{team.average_age}"
+    puts "They play at #{team.stadium}"
   end
 
 end
